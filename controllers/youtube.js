0bcf05418eecd5d7a7c9b0b56431e0ff118fb921
Ian Fitzgerald <ifordfitz@gmail.com>
2019-06-19 20:19:18 -0400

    backend first commit

--- controllers/youtube.js
+++ controllers/youtube.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const model = require('../models/youtube.js')
 
 router.post('/', (req, res)=>{
-  model.create(req.body, (createdElement, error) => {
+  model.create(req.body, (error, createdElement) => {
     res.json(createdElement)
   })
 });
@@ -11,5 +11,12 @@ router.post('/', (req, res)=>{
 router.get('/', (req,res) => {
   res.send('testing')
 })
+
+
+router.delete('/:id', (req, res)=>{
+    model.findByIdAndRemove(req.params.id, (err, deletedModels)=>{
+        res.json(deletedModels);
+    });
+});
 
 module.exports = router;
